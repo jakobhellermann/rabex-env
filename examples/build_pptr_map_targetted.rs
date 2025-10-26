82a33908a06dd60043d14b85d9d34dc5228f9b6e
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-10-26 12:35:48 +0100

    wip

--- examples/build_pptr_map_targetted.rs
+++ examples/build_pptr_map_targetted.rs
@@ -9,6 +9,7 @@ use rabex::objects::pptr::PathId;
 use rabex_env::Environment;
 use rabex_env::addressables::{AddressablesData, ArchivePath};
 use rabex_env::handle::SerializedFileHandle;
+use rabex_env::resolver::EnvResolver;
 use rayon::iter::{IntoParallelRefIterator, ParallelBridge as _, ParallelIterator};
 use rustc_hash::FxHashMap;
 
@@ -25,13 +26,13 @@ fn main() -> Result<()> {
     let env = utils::find_game("silksong")?.unwrap();
     let addressables = env.addressables()?.unwrap();
 
-    let (_global_files, global_file_map) = global_external_numbering(addressables);
+    let (global_files, global_file_map) = global_external_numbering(addressables);
 
     let start = Instant::now();
     env.load_all_serialized_files()?;
     println!("loaded all files in {:?}", start.elapsed());
 
-    for bundle in [
+    let bundles = [
         "dataassets_assets_assets/dataassets/questsystem/quests.bundle",
         "fsmtemplates_assets_shared.bundle",
         "localpoolprefabs_assets_areapeak.bundle",
@@ -39,16 +40,38 @@ fn main() -> Result<()> {
         "scenes_scenes_scenes/song_01.bundle",
         "sfxstatic_assets_areacogarealibrary.bundle",
         "tk2danimations_assets_water.bundle",
-    ] {
+    ];
+    let bundles = env.addressables_bundles();
+    for bundle in bundles {
+        // let bundle = Path::new(bundle);
+        let bundle = bundle
+            .strip_prefix(env.game_files.base_dir().join(addressables.build_folder()))
+            .unwrap();
+        let target_archive_path = addressables
+            .bundle_main_archive_path(bundle)
+            .unwrap()
+            .to_string();
+
+        let target_global_file_id = global_file_map
+            .iter()
+            .find_map(|(key, val)| (*key == target_archive_path).then_some(*val))
+            .unwrap();
+
         let start = Instant::now();
         let referencing_files = find_referencing_files(&env, bundle)?;
 
         let references = DashMap::new();
         referencing_files.par_iter().try_for_each(|(path, file)| {
-            find_pptr_references(&references, &global_file_map, &file, path)
+            find_pptr_references(
+                &references,
+                &global_file_map,
+                &file,
+                path,
+                target_global_file_id,
+            )
         })?;
 
-        println!("{bundle}");
+        println!("{}", bundle.display());
         println!("- {} object's references computed", references.len());
         println!(
             "- {} total references",
@@ -61,9 +84,28 @@ fn main() -> Result<()> {
         for (path, _) in referencing_files {
             let archive_path = ArchivePath::try_parse(Path::new(&path))?.unwrap();
             let _bundle_path = &addressables.cab_to_bundle[archive_path.bundle];
-            // println!("  - {}", bundle_path.display());
+            // println!("  - {}", _bundle_path.display());
         }
         println!("  Scan took {:?}", start.elapsed());
+
+        if false {
+            for (source, references) in references {
+                let global_file = Path::new(&global_files[source.global_file_id as usize]);
+                let bundle_path = ArchivePath::try_parse(global_file)?
+                    .map(|external| addressables.cab_to_bundle[external.bundle].as_path())
+                    .unwrap_or(global_file);
+                println!("- {} @ {}", source.path_id, bundle_path.display());
+
+                for reference in references {
+                    let global_file = Path::new(&global_files[reference.global_file_id as usize]);
+                    let bundle_path = ArchivePath::try_parse(global_file)?
+                        .map(|external| addressables.cab_to_bundle[external.bundle].as_path())
+                        .unwrap_or(global_file);
+                    println!("  by {} @ {}", reference.path_id, bundle_path.display());
+                }
+            }
+        }
+
         println!();
     }
 
@@ -94,14 +136,14 @@ fn global_external_numbering(
 /// Find all files ('archive:/a/b' and content) that reference the given target bundle
 fn find_referencing_files<'a>(
     env: &'a Environment,
-    target_bundle: &str,
+    target_bundle: &Path,
 ) -> Result<Vec<(String, SerializedFileHandle<'a>)>, anyhow::Error> {
     let Some(addressables) = env.addressables()? else {
         return Ok(Vec::new());
     };
 
     let target_archive_path = addressables
-        .bundle_main_archive_path(Path::new(target_bundle))
+        .bundle_main_archive_path(target_bundle)
         .unwrap();
 
     let mut referencing_files = rabex_env::utils::par_fold_reduce(
@@ -127,7 +169,7 @@ fn find_referencing_files<'a>(
     )?;
     referencing_files.push({
         let archive_path = addressables
-            .bundle_main_archive_path(Path::new(target_bundle))
+            .bundle_main_archive_path(target_bundle)
             .unwrap();
         let file = env.load_addressables_bundle_content(&target_bundle)?;
 
@@ -142,6 +184,7 @@ fn find_pptr_references(
     global_file_map: &FxHashMap<String, u32>,
     file: &SerializedFileHandle,
     archive_path: &str,
+    target_global_file_id: u32,
 ) -> Result<()> {
     let global_file_id = global_file_map[archive_path];
 
@@ -165,6 +208,10 @@ fn find_pptr_references(
                 }
             };
 
+            if global_file_id != target_global_file_id {
+                continue;
+            }
+
             let source_pptr = GlobalPPtr {
                 global_file_id,
                 path_id: pptr.m_PathID,
@@ -175,19 +222,3 @@ fn find_pptr_references(
 
     Ok(())
 }
-
-/*for (source, references) in out {
-    let global_file = Path::new(&global_files[source.global_file_id as usize]);
-    let bundle_path = ArchivePath::try_parse(global_file)?
-        .map(|external| addressables.cab_to_bundle[external.bundle].as_path())
-        .unwrap_or(global_file);
-    println!("- {} @ {}", source.path_id, bundle_path.display());
-
-    for reference in references {
-        let global_file = Path::new(&global_files[source.global_file_id as usize]);
-        let bundle_path = ArchivePath::try_parse(global_file)?
-            .map(|external| addressables.cab_to_bundle[external.bundle].as_path())
-            .unwrap_or(global_file);
-        println!("  by {} @ {}", reference.path_id, bundle_path.display());
-    }
-}*/
